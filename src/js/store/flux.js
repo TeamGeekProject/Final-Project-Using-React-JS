ba742dec20febe07627d71b2970290670d199c00
joaarg86 <johanna.e.escobar@icloud.com>
2023-04-07 01:46:34 +0000

    johanna

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -5,13 +5,37 @@ const getState = ({ getStore, getActions, setStore }) => {
     },
     actions: {
       // Use getActions to call a function within a fuction
-      deletetodolistitem: (index) => {
+      // exampleFunction: () => {
+      // 	getActions().changeColor(0, "green");
+      // },
+      //   loadSomeData: () => {
+      //     /**
+      // 				fetch().then().then(data => setStore({ "foo": data.bar }))
+      // 			*/
+      //   },
+      // changeColor: (index, color) => {
+      //   //get the store
+      //   const store = getStore();
+
+      //   //we have to loop the entire demo array to look for the respective index
+      //   //and change its color
+      //   const demo = store.demo.map((elm, i) => {
+      //     if (i === index) elm.background = color;
+      //     return elm;
+      //   });
+
+      //   //reset the global store
+      //   setStore({ demo: demo });
+      // },
+
+      deleteTodoListItem: (index) => {
         const store = getStore();
         const todoListItems = store.todoListItems.filter(
           (item, i) => i !== index
         );
         setStore({ todoListItems: todoListItems });
       },
+
       addTodoListItem: (item) => {
         const store = getStore();
         const todoListItems = store.todoListItems.concat(item);
@@ -20,4 +44,5 @@ const getState = ({ getStore, getActions, setStore }) => {
     },
   };
 };
+
 export default getState;
--- src/js/views/todo.js
+++ src/js/views/todo.js
@@ -1,57 +1,8 @@
-// import React, { useState, useEffect, useContext } from "react";
-import { Link } from "react-router-dom";
-
-// import { Context } from "../store/appContext";
-
-// import "../../styles/demo.css";
-
-// export const Demo = () => {
-//   const { store, actions } = useContext(Context);
-
-//   return (
-//     <div className="container">
-//       {/* <ul className="list-group">
-//         {store.demo.map((item, index) => {
-//           return (
-//             <li
-//               key={index}
-//               className="list-group-item d-flex justify-content-between"
-//               style={{ background: item.background }}
-//             >
-//               <Link to={"/single/" + index}>
-//                 <span>Link to: {item.title}</span>
-//               </Link>
-//               {
-//                 // Conditional render example
-//                 // Check to see if the background is orange, if so, display the message
-//                 item.background === "orange" ? (
-//                   <p style={{ color: item.initial }}>
-//                     Check store/flux.js scroll to the actions to see the code
-//                     ---- SECOND PAGE
-//                   </p>
-//                 ) : null
-//               }
-//               <button
-//                 className="btn btn-success"
-//                 onClick={() => actions.changeColor(index, "orange")}
-//               >
-//                 Change Color
-//               </button>
-//             </li>
-//           );
-//         })}
-//       </ul> */}
-//       <br />
-//       <Link to="/home">
-//         <button className="btn btn-primary">Back home</button>
-//       </Link>
-//     </div>
-//   );
-// };
 import { Context } from "../store/appContext";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 import React, { useState, useEffect, useContext } from "react";
+
 import "../../styles/todo.css";
 
 export const Todo = () => {
@@ -109,10 +60,6 @@ export const Todo = () => {
           </div>
         </div>
       </div>
-      <br />
-      <Link to="/home">
-        <button className="btn btn-primary">Back home</button>
-      </Link>
     </div>
   );
 };
