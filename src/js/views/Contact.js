82a5007d424884e326f69e9d3b5d1f4e5f8926c0
spacemc2 <mtorresroadby@gmail.com>
2023-04-23 16:47:20 -0400

    fixing problems  with listing contacts

--- src/js/views/Contact.js
+++ src/js/views/Contact.js
@@ -10,32 +10,25 @@ export const Contact = () => {
   const { store, actions } = useContext(Context);
 
   return (
-    <>
+    <div>
       <h1>Archived Contacts</h1>
+      {store.todoListItems.map((item, index) => (
+        <li className="list-group-item rounded" key={index}>
+          <span className="float-start items-style">{item}</span>
+          <span
+            className="float-end text-danger"
+            onClick={() => {
+              actions.deleteTodoListItem(index);
+            }}
+          >
+            {" "}
+            <h1 className="x">X</h1>
+            {/* <FontAwesomeIcon icon={faXmarksLines}></FontAwesomeIcon> */}
+          </span>
 
-      <div className="jumbotron">
-        <h1 className="display-4">
-          <ul className="list-group ">
-            {store.todoListItems.map((item, index) => (
-              <li className="list-group-item rounded" key={index}>
-                <span className="float-start items-style">{item}</span>
-                <span
-                  className="float-end text-danger"
-                  onClick={() => {
-                    actions.deleteTodoListItem(index);
-                  }}
-                >
-                  {" "}
-                  <h1 className="x">X</h1>
-                  {/* <FontAwesomeIcon icon={faXmarksLines}></FontAwesomeIcon> */}
-                </span>
-
-                <hr className="my-4" />
-              </li>
-            ))}
-          </ul>
-        </h1>
-      </div>
-    </>
+          <hr className="my-4" />
+        </li>
+      ))}
+    </div>
   );
 };
